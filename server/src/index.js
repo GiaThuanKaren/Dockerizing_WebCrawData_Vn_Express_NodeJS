a6fce22afd152a3e9c26f03a61da4c9413792d55
GiaThuanKaren <GiaThuanNguyen213@gmail.com>
2022-06-29 09:18:10 +0700

    Update : Now we can get news from every tittle catologe 29/6/2022

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 const PORT = 5000;
-
+let ARR_TYPE = [];
 const cheerio = require("cheerio"); // khai báo module cheerio
 
 const request = require("request-promise");
@@ -46,22 +46,27 @@ app.get("/catologe", (req, res) => {
   request("https://vnexpress.net/", (err, response, html) => {
     if (!err && response.statusCode == 200) {
       const $ = cheerio.load(html);
-      let i=0,arr=[];
-      const header = $('.main-nav li').each((index,ele)=>{
+      let i = 0,
+        arr = [];
+      const header = $(".main-nav li").each((index, ele) => {
         const id = $(ele).attr("data-id");
-        const className =$(ele).find("a");
-        const href = className.attr("href")
+        const className = $(ele).find("a");
+        const href = className.attr("href");
         const tittleCatologe = $(ele).text();
-        console.log(href)
-        arr =[...arr,{
-          id:id ? id: "",
-          name:tittleCatologe.replace(/\s/g,''),
-          link:href
-        }]
+        console.log(href);
+        arr = [
+          ...arr,
+          {
+            id: id ? id : "",
+            name: tittleCatologe.replace(/\s/g, ""),
+            link: href,
+          },
+        ];
         // const tittle = $(el).text()
         i++;
-      })
-      res.json(arr)
+      });
+      ARR_TYPE = arr;
+      res.json(arr);
       // console.log(i)
     } else {
       console.log(err);
@@ -69,17 +74,46 @@ app.get("/catologe", (req, res) => {
     }
   });
 });
-
-app.get("/query",async (req,res)=>{
-  const {search }=req.query
-  console.log(req.query )
+// https://vnexpress.net/giao-thong-lon-xon-tren-tuyen-buyt-nhanh-brt-4481140.html#box_comment_vne
+// https://vnexpress.net/giao-thong-lon-xon-tren-tuyen-buyt-nhanh-brt-4481140.html
+app.get("/query/:string", async (req, res) => {
+  console.log(req.params);
+  let check = ARR_TYPE.find((item) => {
+    return item.link == `/${req.params.string}`;
+  });
+  if (check) {
+    request(`https://vnexpress.net/${req.params.string}`, (err, response, html) => {
+      if (!err && response.statusCode == 200) {
+        let i=0,result=[];
+        const $ = cheerio.load(html);
+        const news = $('.col-left-folder-v2 article').each((idx,ele)=>{
+          const tittlenews = $(ele).find(".title-news")
+          const TagA=tittlenews.find("a").attr("href");
+          const Name=tittlenews.text(); 
+          const description =  $(ele).find(".description").text();
+          // console.log(description.replace(/\n/g, " "))
+          i++;
+          result=[...result,{
+            name:Name.replace(/\n/g, " "),
+            description:description.replace(/\n/g, " "),
+            link:TagA
+          }]
+        })
+        // console.log(result)
+        res.json(result);
+      } else {
+        res.send("Error Respones");
+      }
+    });
+  } else {
+    res.send("Invalid Param");
+  }
   // let getrespone=await fetch('http://localhost:5000/catologe');
   // let catologeArr= await getrespone.json();
   // console.log(catologeArr)
 
-  res.send("hiu")
-})
-
+  // res.send("hiu");
+});
 
 app.listen(PORT, () => {
   console.log("Listenning port " + PORT);
